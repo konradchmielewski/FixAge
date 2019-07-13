02ef686edec64e11e63cf38df52c640f1497066b
Konrad Chmielewski <44814786+konradchmielewski@users.noreply.github.com>
2019-07-13 19:44:25 +0200

    Add files via upload
    initial commit

---
+++ FixAge/FixAge/wwwroot/js/fixAge.js
@@ -0,0 +1,15 @@
+﻿var msg = ([5, 15, 25, 78, 59, 45, 75]);
+var fixedAgeString;
+
+// Function filters array elements that are less than (or even) 18 and more than (or even) 60. Then it returns the result as string. 
+function fixAge(a) {
+    var filtered = a.filter(function (value, index, arr) {
+        return value >= 18 & value <= 60;
+    });
+
+    fixedAgeString = filtered.join(", ");
+    return fixedAgeString;
+}
+
+fixAge(msg);
+console.log(fixedAgeString); 
